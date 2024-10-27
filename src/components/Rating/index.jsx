fe1d87317fbb67d1353ab2205ee86a636bd434e6
Klara Sejnova <k.sejnova@gmail.com>
2024-10-27 12:55:02 +0100

    final

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
-import { Star } from '../Star';
-import './style.css';
+import { useState } from "react";
+import { Star } from "../Star";
+import "./style.css";
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
-  
+  const Select = (val) => {
+    setRating(val);
+  };
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} onSelect={Select} glowing={rating >= 1} />
+        <Star value={2} onSelect={Select} glowing={rating >= 2} />
+        <Star value={3} onSelect={Select} glowing={rating >= 3} />
+        <Star value={4} onSelect={Select} glowing={rating >= 4} />
+        <Star value={5} onSelect={Select} glowing={rating == 5} />
       </div>
     </div>
   );
